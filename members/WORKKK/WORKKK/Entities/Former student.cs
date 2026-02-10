ab36d5913446aed34bcb5a949a7a59ed4a7540da
sayuxsoul <angelzarite9@gmail.com>
2026-02-10 18:42:07 -0400

    Add files via upload

---
+++ members/WORKKK/WORKKK/Entities/Former student.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WORKKK.Entities
+{
+    internal class Former_student:Communitymembers
+    {
+        public int GraduationYear { get; set; }
+
+        public string Degree {  get; set; }
+    }
+}
